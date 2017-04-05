1e495b7bee67b8cffa78cf9b452ce350ecb6e06e
Joel Zhou <jun.zhou3@hpe.com>
2017-04-05 10:47:36 +0800

    changed to vpc-propel org

--- start-migration.sh
+++ start-migration.sh
@@ -10,9 +10,9 @@ do
        git clone $sourceURL
        cd $repoName
        (git branch -r | sed -n '/->/!s#^  origin/##p' && echo master) | xargs -L1 git checkout
-       git remote add dxcgithub git@github.houston.entsvcs.net:VPC-RnD/${repoName}-origin.git
-       curl -XDELETE -H "Authorization: token f637dcc729d22bdaa6a3519336475b912f3efc69" https://github.houston.entsvcs.net/api/v3/repos/VPC-RnD/${repoName}-origin
-       curl -XPOST -H "Authorization: token f637dcc729d22bdaa6a3519336475b912f3efc69" https://github.houston.entsvcs.net/api/v3/orgs/VPC-RnD/repos -d '{"name":"'${repoName}-origin'", "description":"'${repoName}'"}' 1>/dev/null 2>&1
+       git remote add dxcgithub git@github.houston.entsvcs.net:vpc-propel/${repoName}.git
+       curl -XDELETE -H "Authorization: token f637dcc729d22bdaa6a3519336475b912f3efc69" https://github.houston.entsvcs.net/api/v3/repos/vpc-propel/${repoName}
+       curl -XPOST -H "Authorization: token f637dcc729d22bdaa6a3519336475b912f3efc69" https://github.houston.entsvcs.net/api/v3/orgs/vpc-propel/repos -d '{"name":"'${repoName}'", "description":"'${repoName}'"}' 1>/dev/null 2>&1
        git push --all dxcgithub
        git push --tags dxcgithub
        cd ..
